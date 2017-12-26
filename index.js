a803b63ae8a060e874e45899041397cc7f5dc636
Sachin Gaikwad <gaikwad411@gmail.com>
2017-12-26 12:32:35 +0530

    add tasks to list when enter is pressed on input text also clear the input when esc is pr :wq`

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ const path = require('path');
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'index.html')));
 
+app.get('/jquery.js', (req, res) => res.sendFile(path.join(__dirname, 'node_modules', 'jquery', 'dist', 'jquery.min.js')));
+
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
